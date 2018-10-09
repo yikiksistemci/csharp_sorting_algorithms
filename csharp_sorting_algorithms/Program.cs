0849ac123bdb176e4e53b7ce8ef0988f645282ab
Yunus YAŞAR <yunusy20@gmail.com>
2018-10-08 20:23:54 -0400

    first commit

--- csharp_sorting_algorithms/Program.cs
+++ csharp_sorting_algorithms/Program.cs
@@ -1,9 +1,149 @@
-﻿namespace csharp_sorting_algorithms
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+
+
+
+namespace csharp_sorting_algorithms
 {
     internal class Program
     {
+        static string insertion_siralamasi_baslangic_zamani=String.Empty;
+        static string quicksort_siralamasi_baslangic_zamani=String.Empty;
+        static string bubblesort_siralamasi_baslangic_zamani=String.Empty;
+        
+        
+        
         public static void Main(string[] args)
         {
         }
+
+
+
+
+
+
+        static List<int> insertion_siralamasi(List<int> numara_deposu)
+        {
+
+            if (string.IsNullOrEmpty(insertion_siralamasi_baslangic_zamani))
+                insertion_siralamasi_baslangic_zamani = DateTime.Now.ToLongDateString();
+
+
+            for (int j = 1; j < numara_deposu.Count; j++)
+            {
+                int deger = numara_deposu[j];
+                int i = j - 1;
+                while (i >=0 && numara_deposu[i] > deger)
+                {
+                    numara_deposu[i + 1] = numara_deposu[i];
+                    i = i - 1;
+                    
+
+                }
+
+                numara_deposu[i + 1] = deger;
+
+            }
+
+            return numara_deposu;
+        }
+
+
+
+
+
+
+
+        static List<int> quicksort_siralamasi(List<int> numara_deposu, int solDeger, int sagDeger)
+        {
+            if (string.IsNullOrEmpty(quicksort_siralamasi_baslangic_zamani))
+            {
+                quicksort_siralamasi_baslangic_zamani = DateTime.Now.ToLongDateString();
+            }
+
+            int i = solDeger;
+            int j = sagDeger;
+            double pivotDeger = ((solDeger + sagDeger) / 2);
+            int x = numara_deposu[Convert.ToInt32(pivotDeger)];
+
+            int w = 0;
+            while (i<=j)
+            {
+
+                while (numara_deposu[i]<x)
+                {
+                    i++;
+
+                }
+
+                while (x<numara_deposu[j])
+                {
+
+                    j--;
+
+                }
+
+                if (i<=j)
+                {
+
+                    w = numara_deposu[i];
+
+                    numara_deposu[i++] = numara_deposu[j];
+                    numara_deposu[j] = w;
+
+                }
+               
+            }
+
+            if (solDeger<j)
+            {
+
+                quicksort_siralamasi(numara_deposu, solDeger, j);
+
+            }
+
+            if (i<sagDeger)
+            {
+
+                quicksort_siralamasi(numara_deposu, i, sagDeger);
+
+            }
+
+            return numara_deposu;
+
+
+
+        } 
+        
+        
+        
+        
+
+
+        static List<int> RandomSayiUret(int sinir, int baslangic, int bitis)
+        {
+            List<int> yigin=new List<int>();
+            Random rast=new Random();
+
+            for (int i = 0; i <sinir ; i++)
+            {
+                yigin.Add(rast.Next(baslangic,bitis));
+                
+            }
+
+            return yigin;
+
+        }
+        
+        
+        
+        
+        
+        
     }
 }
